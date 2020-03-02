9f3c6047d944a5c2a816319c3c6680d21be65906
Ualas Lima <ualaslima@outlook.com>
2020-03-02 02:09:02 -0300

    Correção de bugs - projeto finalizado - sem bugs até que seja encontrados haha

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
-var operando1 = null;
-var operando2 = null;
+var operando1 = 0;
+var operando2 = 0;
 var operacao = null;
-var aux = null;
-var casaDecimal = false;
+var startOperando1 = false;
+var startOperando2 = false;
+var operador = false;
 var id;
 var resultado = null;
+var contVirgulaOperando1 = 0;
+var contVirgulaOperando2 = 0;
 
 //A função nos fornece o id do elemento clicado e a partir dai verificamos como proceder
 //Caso o valor seja uma virgula e a condição de validade seja atendida a chave casaDecimal é setada como true
 function tratamento(id){
-    if (id == "virgula") {
-        if(operando1 == null && operando2 == null)
-            alert("use a virgula somente após digitar o valor inteiro");
-        else if(operando1 != null || operando2 != null)
-            casaDecimal = true;
-        }
-
-    //Caso o elemento clicado seja uma operação (a operaçao será realizada fora da função pois ela é inserida pelo usuário antes do operando2)    
-    else if(id == "adicao" || id == "subtracao" || id == "multiplicacao" || id == "divisao") {
+    if ((id == "virgula" && (startOperando1 == false && startOperando2 == false)) || (id == "virgula" && startOperando2 == false && operador == true) || (startOperando1 == true && resultado != null && id == "virgula" && operador == false))
+        alert("Use a virgula somente após digitar o valor inteiro");
+    else if((id == "adicao" || id == "subtracao" || id == "multiplicacao" || id == "divisao") && startOperando1 == false)
+        alert("Use um operador apenas após inserir o primeiro oprerando");
+    else if((id == "adicao" || id == "subtracao" || id == "multiplicacao" || id == "divisao") && startOperando1 == true && startOperando2 == true)
+        alert("Atenção: Uma operação por vez, tecle ENTER para o resultado");
+    else if((id == "adicao" || id == "subtracao" || id == "multiplicacao" || id == "divisao") && startOperando1 == true && startOperando2 == false) {
+		operador = true;
         switch(id) {
             case 'adicao':
                 operacao = 'adicao';
-                document.getElementById('visor').placeholder = (document.getElementById('visor').placeholder + '+');
+                document.getElementById('visor').placeholder = operando1 + '+';
                 break;
             case 'subtracao':
                 operacao = 'subtracao';
-                document.getElementById('visor').placeholder = (document.getElementById('visor').placeholder + '-');
+                document.getElementById('visor').placeholder = operando1 + '-';
                 break;
             case 'multiplicacao':
                 operacao = 'multiplicacao';
-                document.getElementById('visor').placeholder = (document.getElementById('visor').placeholder + '*');
+                document.getElementById('visor').placeholder = operando1 + '*';
                 break;
             case 'divisao':
                 operacao = 'divisao';
-                document.getElementById('visor').placeholder = (document.getElementById('visor').placeholder + '/');
+                document.getElementById('visor').placeholder = operando1 + '/';
                 break;
             default:
                 document.getElementById("visor").placeholder = 'ERRO'
@@ -42,54 +44,64 @@ function tratamento(id){
 
 
     }
-
-    //Caso o elemento seja um numeral
     else {
-        //Verifica se o valor inserido é para o operando1
-        if(operando1 == null)  {
-            operando1 = document.getElementById(id).value;    
-            operando1 = parseFloat(operando1);
-            document.getElementById('visor').placeholder = operando1;
+        console.log("id é " + id);
+        if (resultado != null && operador == false) {
+            resultado = null;
+            startOperando1 = false;
+            operando1 = 0;
+            contVirgulaOperando1 = 0;
         }
-        //Verifica se o usuário deseja uma casa decimal para o operando1
-        else if(casaDecimal == true && operando2 == null) {
-            aux = document.getElementById(id).value;
-            //necessário converter o operando1 de volta para string para poder fazer uma concatenação
-            operando1 = String(operando1);
-            operando1 = operando1 + aux;
-            aux = null;
-            //Agora novamente fazemos com q o valor se torne númerico para ter o decimal
-            operando1 = parseFloat(operando1);
-            //Dividimos o valor por 10 para ter o decimal, a formula é que a concatenação de A e B dividido por 10 dará o valor decimal A,B
-            operando1 = (operando1 / 10);
-            casaDecimal = false;
-            document.getElementById('visor').placeholder = operando1;
-        }
-        //Verifica se o valor inserido é para operando2
-        else if(casaDecimal == false && operando2 == null) {
-            operando2 = document.getElementById(id).value;    
-            operando2 = parseFloat(operando2);
-            document.getElementById('visor').placeholder = (document.getElementById('visor').placeholder + operando2);
-        }
-        //Verifica se o usuário deseja uma casa decimal para o operando2        
-        else {
-            aux = document.getElementById(id).value;
-            //necessário converter o operando2 de volta para string para poder fazer uma concatenação
-            operando2 = String(operando2);
-            operando2 = operando2 + aux;
-            aux = null;
-            //Agora novamente fazemos com q o valor se torne númerico para ter o decimal
-            operando2 = parseFloat(operando2);
-            //Dividimos o valor por 10 para ter o decimal, a formula é que a concatenação de A e B dividido por 10 dará o valor decimal A,B
-            operando2 = (operando2 / 10);
-            casaDecimal = false;
-            document.getElementById('visor').placeholder = (operando1 + ' op ' + operando2);
-        }
+		if(operador == false) {
+            if(id == "virgula") {
+                contVirgulaOperando1++;
+                console.log("algorismo inserido "+id);
+            }
+            else
+                console.log("algorismo inserido "+id);
+            if(contVirgulaOperando1 < 2 || id != "virgula"){
+                if(startOperando1 == false) {
+                    operando1 = document.getElementById(id).value;
+                    document.getElementById('visor').placeholder = operando1;   
+                    startOperando1 = true; 
+                }
+                else {
+                operando1 = operando1 + document.getElementById(id).value;
+                document.getElementById('visor').placeholder = operando1;
+                }
+            }
+            else
+                console.log("Tentativa de inserir mais de uma virgula no operando1");
+		}
+		else {
+            if(id == "virgula") {
+                contVirgulaOperando2++;
+                console.log("algorismo inserido "+id);
+            }
+            else
+                console.log("algorismo inserido "+id);
+            if(contVirgulaOperando2 < 2 || id != "virgula"){
+                if (operando2 == false) {
+                    operando2 = document.getElementById(id).value;
+                    document.getElementById('visor').placeholder = document.getElementById('visor').placeholder + operando2;    
+                    startOperando2 = true;
+                }
+                else {
+                    operando2 = operando2 + document.getElementById(id).value;
+                    document.getElementById('visor').placeholder = document.getElementById('visor').placeholder + document.getElementById(id).value;
+                }
+            }
+            else
+            console.log("Tentativa de inserir mais de uma virgula no operando2");
+		}       
     }
 }
 
 //Operações e resultados quando o botão ENTER (submit) for clicado
 function op() {
+    contVirgulaOperando2 = 0;
+	operando1 = parseFloat(operando1);
+	operando2 = parseFloat(operando2);
     switch(operacao) {
         case 'adicao':
             resultado = operando1 + operando2;
@@ -105,15 +117,23 @@ function op() {
             break;
         default:
             document.getElementById("visor").placeholder = 'ERRO'
+            operando1 = 0;
+            operando2 = 0;
+            operacao = null;
+            startOperando1 = false;
+            startOperando2 = false;
+            operador = false;
+            id;
+            resultado = null;
+            contVirgulaOperando1 = 0;
+            contVirgulaOperando2 = 0;
             return false;
     }
 
     document.getElementById('visor').placeholder = resultado;
-    operando1 = resultado;
-    operando2 = null;
+    operando1 = String(resultado);
+    operando2 = 0;
     operacao = null;
-    aux = null;
-    casaDecimal = false;
-    id;
-    resultado = null;
+    operador = false;
+    startOperando2 = false;
 }
